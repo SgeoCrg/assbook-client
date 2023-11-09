f8c08a540fad482e52d4edcf07871853e58ffb77
SgeoCrg <sgeo_crg@hotmail.com>
2023-11-09 22:44:07 +0100

    funcionando como la anterior version

--- src/add-post.ts
+++ src/add-post.ts
@@ -3,31 +3,34 @@
 import { PostsService } from "./classes/posts-service.ts";
 import { Post } from "./interfaces/post.ts";
 import { MapService } from "./classes/map-service.ts";
+import { MyGeolocation } from "./classes/my-geolocation.ts";
+import { Coordinates } from "./interfaces/coordinates.ts";
 
 const postsService = new PostsService();
-const mapServices = new MapService();
 const newPostForm = document.getElementById("newPlace") as HTMLFormElement;
 const errorMsg = document.getElementById("errorMsg");
 const imgPreview = document.getElementById("imgPreview");
-//const place = document.getElementById("place");
-//const locationContainer = document.getElementById("location-container");
+const place = document.getElementById("place");
+const locationContainer = document.getElementById("location-container");
 
 async function validateForm(event: Event): Promise<void> {
     event.preventDefault();
-    const place = newPostForm.place.value.trim();
-    const title = newPostForm.title.trim();//title.value.trim();
+    let place = newPostForm.place.value.trim();
+    const title = (newPostForm.elements.namedItem("title") as HTMLInputElement).value;//newPostForm.title.value.trim();
     const image: string = newPostForm.image.value ? (<HTMLImageElement>imgPreview).src : "";
     const description = newPostForm.description.value.trim();
     const mood = +newPostForm.mood.value;
     const lat = +newPostForm.lat.value;
     const lng = +newPostForm.lng.value;
 
-    if (!title && !description && (!image && !place)) {
+    if (!title && !description && (!image && (!place && !document.getElementById("lat")?.classList.contains("on")))) {
         errorMsg!.classList.remove("hidden");
         setTimeout(() => errorMsg!.classList.add("hidden"), 3000);
     } else {
         try {
             if(image == "") {
+                if(place == "" && document.getElementById("lat")?.classList.contains("on"))
+                    place = "Mi ubicacion";
                 console.log(title, description, mood, lat, lng, place);
                 await postsService.post({
                     title, 
@@ -51,33 +54,34 @@ const radioButGroup = document.getElementsByName("type");
 for(let i = 0; i < radioButGroup.length; i++) {
     radioButGroup[i].addEventListener("change", e => {
         if((<HTMLInputElement>e.srcElement!).id==="postLocation") {
+            switchAddress(true);
             document.getElementById("photo-group")!.classList.add("d-none");
             document.getElementById("location-group")!.classList.remove("d-none");
-            //mapServices.showMap();//getMap()
-            loadBingApi();
+            showMap();//mapServices.showMap();//getMap()
         } else {
             document.getElementById("photo-group")!.classList.remove("d-none");
             document.getElementById("location-group")!.classList.add("d-none");
+            switchAddress(false);
         }
     } );
 } 
 
 function loadImage(event: Event): void {
-    const file = (event.target)!.files[0];
+    const file = (event.target as HTMLInputElement).files![0];
     const reader = new FileReader();
 
     if (file) reader.readAsDataURL(file);
 
     reader.addEventListener("load", () => {
-        (<HTMLImageElement>imgPreview).classList.remove("d-none");
-        (<HTMLImageElement>imgPreview).src = reader.result;
+        imgPreview!.classList.remove("d-none");
+        (<HTMLImageElement>imgPreview)!.src = reader.result as string;
     });
 }
 
 function createScript(): void {
     const script = document.createElement("script");
     //let body = document.getElementsByTagName("body");
-    script.src="http://www.bing.com/api/maps/mapcontrol?callback=showMap";//=getMap
+    script.src="http://www.bing.com/api/maps/mapcontrol?callback=getMap";//=showMap
     script.defer = true;
     document.body.append(script);
 }
@@ -96,7 +100,46 @@ function createHiddenFields(): void {
     form!.append(lng);
 }
 
+async function showMap(): Promise<void> {
+    const coords = await MyGeolocation.getLocation();
+    //const lat = document.getElementById("lat");
+    //const lng = document.getElementById("lng");
+    //(<HTMLInputElement>lat).value = coords.latitude.toString();
+    //(<HTMLInputElement>lng).value = coords.longitude.toString();
+    coordinatesMap(coords);
+    const mapsService = await MapService.createMapService(coords, "map");
+    const marker = mapsService.createMarker(coords, "red");
+    const autosuggest = await mapsService.getAutoSuggestManager();
+    autosuggest.attachAutosuggest("#place", "#location-container", (result) => {//("#search", "#searchcontainer",
+        marker.setLocation(result.location);
+        mapsService.map.setView({center: result.location});
+        //(<HTMLInputElement>lat).value = result.location.latitude.toString();
+        //(<HTMLInputElement>lng).value = result.location.longitude.toString();
+        coordinatesMap(result.location);
+    });
+}
+
+function coordinatesMap(coords: Coordinates): void {
+    const lat = document.getElementById("lat");
+    const lng = document.getElementById("lng");
+    (<HTMLInputElement>lat).value = coords.latitude.toString();
+    (<HTMLInputElement>lng).value = coords.longitude.toString();
+}
+
+function switchAddress(bool: boolean): void {
+    const lat = document.getElementById("lat");
+    const lng = document.getElementById("lng");
+    if(bool) {
+        lat?.classList.add("on");
+        lng?.classList.add("on");
+    } else {
+        lat?.classList.remove("on");
+        lng?.classList.remove("on");
+    }
+}
+
 newPostForm.image.addEventListener("change", loadImage);
 newPostForm!.addEventListener("submit", validateForm);
+
 createScript();
 createHiddenFields();
--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const SERVER = "https://api.fullstackpro.es/assbook-lite";//quitar lite
-export const API_KEY = "Bing maps API key";
+export const API_KEY = "Atvu8CHBTIby7K74kcdMvqYXgwo00iZhMnIhgXBXwVhq7VwknG0sbS2a1Sv_TjaW";
